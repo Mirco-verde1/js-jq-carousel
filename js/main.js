bea71a7b3127a1db597cd7916109a3bdbdb727c4
Mirco <verderosamircowork@gmail.com>
2021-01-14 11:12:16 +0100

    Ciclo loop infinito nello scorrimento immagini e point

--- js/main.js
+++ js/main.js
@@ -1,26 +1,52 @@
+
 $(document).ready(function() {
 
 
+//cliccando sul pulsante next(freccia destra del carousel) scorre le immagini
+//Quando arriverà all'ultima immagine ricomincerà dalla prima
+//Stessa evento accadrà nei point sotto il carousel
   $('.next > i').click(function() {
-    $('.images img.active').removeClass('active').next().addClass('active');
-  })
+
+
+      if ($('img.active, i.active').hasClass('last')) {
+
+         $('img.active, i.active').removeClass('active');
+         $('img.first , i.first').addClass('active')
+
+      }else {
+        $('img.active, i.active').removeClass('active').next().addClass('active')
+      }
+
+
+  });
+
+  //cliccando sul pulsante prev(freccia sinistra del carousel) scorre le immagini
+  //Quando arriverà alla prima immagine ricomincerà dall'ultima
+  //Stessa evento accadrà nei point sotto il carousel
 
   $('.prev > i').click(function() {
-    $('.images img.active').removeClass('active').prev().addClass('active');
-  })
 
 
-  //
-  //
-  // $( ".next > i" ).click(function() {
-  //   if ( $('.images:last-child') ) {
-  //
-  //     $('.images:first-child');
-  //
-  //   }
-  // });
+      if ($('img.active, i.active').hasClass('first')) {
+
+         $('img.active, i.active').removeClass('active');
+         $('img.last, i.last').addClass('active')
+
+      }else {
+        $('img.active, i.active').removeClass('active').prev().addClass('active')
+      }
+
+
+  });
+
+
+
+
+
+
+
 
 
 
 //FINE DOCUMENT.READY
-})
+});
