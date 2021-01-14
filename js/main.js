a836fe82cba161d307794a5f3f39c5b812f863be
Mirco <verderosamircowork@gmail.com>
2021-01-14 12:10:12 +0100

    Aggiunta funzioni

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+//POSSIAMO UTILIZZARE DELLE FUNZIONI CHE RICHIAMIAMO NEL DOCUMENT.ready
+//UTILIZZO UNA FUNZIONE PIU SPECIFICA CHE UTILIZZO SOLO IN QUETA APPLICAZIONE
+
+
+
+function nextImage(){
+
+        if ($('img.active, i.active').hasClass('last')) {
+
+           $('img.active, i.active').removeClass('active');
+           $('img.first , i.first').addClass('active')
+
+        }else {
+          $('img.active, i.active').removeClass('active').next().addClass('active')
+        }
+}
+
+
+
+
+function prevImage(){
+
+  if ($('img.active, i.active').hasClass('first')) {
+
+     $('img.active, i.active').removeClass('active');
+     $('img.last, i.last').addClass('active')
+
+  }else {
+    $('img.active, i.active').removeClass('active').prev().addClass('active')
+  }
+
+}
+
 
 $(document).ready(function() {
 
@@ -5,17 +38,22 @@ $(document).ready(function() {
 //cliccando sul pulsante next(freccia destra del carousel) scorre le immagini
 //Quando arriverà all'ultima immagine ricomincerà dalla prima
 //Stessa evento accadrà nei point sotto il carousel
+//HO COMMENTATO IL CODICE SOSTITUITO DALLA FUNZIONE
   $('.next > i').click(function() {
 
+    nextImage();
 
-      if ($('img.active, i.active').hasClass('last')) {
 
-         $('img.active, i.active').removeClass('active');
-         $('img.first , i.first').addClass('active')
 
-      }else {
-        $('img.active, i.active').removeClass('active').next().addClass('active')
-      }
+      //
+      // if ($('img.active, i.active').hasClass('last')) {
+      //
+      //    $('img.active, i.active').removeClass('active');
+      //    $('img.first , i.first').addClass('active')
+      //
+      // }else {
+      //   $('img.active, i.active').removeClass('active').next().addClass('active')
+      // }
 
 
   });
@@ -23,18 +61,19 @@ $(document).ready(function() {
   //cliccando sul pulsante prev(freccia sinistra del carousel) scorre le immagini
   //Quando arriverà alla prima immagine ricomincerà dall'ultima
   //Stessa evento accadrà nei point sotto il carousel
-
+//HO COMMENTATO IL CODICE SOSTITUITO DALLA FUNZIONE
   $('.prev > i').click(function() {
 
+    prevImage();
 
-      if ($('img.active, i.active').hasClass('first')) {
-
-         $('img.active, i.active').removeClass('active');
-         $('img.last, i.last').addClass('active')
-
-      }else {
-        $('img.active, i.active').removeClass('active').prev().addClass('active')
-      }
+      // if ($('img.active, i.active').hasClass('first')) {
+      //
+      //    $('img.active, i.active').removeClass('active');
+      //    $('img.last, i.last').addClass('active')
+      //
+      // }else {
+      //   $('img.active, i.active').removeClass('active').prev().addClass('active')
+      // }
 
 
   });
